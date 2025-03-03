b529390c18946b21761576c3c7a4e133d293d71f
John Feltz <johncfeltz@gmail.com>
2025-03-03 09:41:44 -0500

    Adds basic rock band instruments

---
+++ db/migrate/20250303143750_load_instruments.rb
@@ -0,0 +1,12 @@
+class LoadInstruments < ActiveRecord::Migration[8.0]
+  def up
+    Instrument.create(name: "Drums")
+    Instrument.create(name: "Bass")
+    Instrument.create(name: "Guitar 1")
+    Instrument.create(name: "Guitar 2")
+    Instrument.create(name: "Keyboards 1")
+    Instrument.create(name: "Keyboards 2")
+    Instrument.create(name: "Vocals 1")
+    Instrument.create(name: "Vocals 2")
+  end
+end
