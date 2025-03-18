81409a5b08627f7954f2f9a7e818d29238873791
John Feltz <johncfeltz@gmail.com>
2025-03-18 13:05:04 -0400

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,22 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Instrument.create(name: "Drums")
+Instrument.create(name: "Bass")
+Instrument.create(name: "Guitar 1")
+Instrument.create(name: "Guitar 2")
+Instrument.create(name: "Keyboards 1")
+Instrument.create(name: "Keyboards 2")
+Instrument.create(name: "Vocals 1")
+Instrument.create(name: "Vocals 2")
+Instrument.create(name: "Harmonica")
+Instrument.create(name: "Saxophone")
+Instrument.create(name: "Trumpet")
+Instrument.create(name: "Trombone")
+Instrument.create(name: "Drums 2")
+Instrument.create(name: "Engineer")
+
+b = Band.create(name: "Silver Lake Band")
+
+Player.create(first_name: "John", last_name: "Feltz", band_id: b.id)
